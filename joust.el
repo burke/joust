09a04f4a04a3f15e10d3fa5ab6f703525124b57c
Burke Libbey <burke@burkelibbey.org>
2009-01-08 22:01:23 -0600

    made joust.el better

--- joust.el
+++ joust.el
@@ -7,26 +7,22 @@
 ;; Version: 0.1
 ;; Keywords: tools
 
-;; This file is not (yet) part of GNU Emacs.
-;; However, it is distributed under the same license.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
+;; 
+;; You should have received a copy of the GNU General Public
+;; License along with this program; if not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+;; MA 02111-1307, USA.
 
 ;;; Code:
-
 
 (unless (boundp '*emacs-config-directory*)
   (defvar *emacs-config-directory* "~/.emacs.d"))
@@ -44,15 +40,15 @@
   (let ((packages (directory-files *joust-packages-directory*)))
     (remove "." (remove ".." packages))))
 
-(defun joust:require-packages (packages)
-  "Given a list of package names, load all of them from the packages directory"
-  (if (not (eq nil packages))
-    (let ((package (car packages)))
-      (joust:add-path package)
-      (require (intern package))
-      (joust:require-packages (cdr packages)))))
+(defun joust:require-package (package)
+  "Given a package name, load it from the packages directory"
+  (joust:add-path package)
+  (require (intern package)))
 
-(joust:require-packages (joust:list-packages))
+(defun joust:initialize ()
+  "Load all packages"
+  (mapcar 'joust:require-package (joust:list-packages)))
 
+(joust:initialize)
 (provide 'joust)
 
