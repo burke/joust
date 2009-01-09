ab18a6559bce0196138f2bf1d25a9bc9a9ff5f06
Burke Libbey <burke@burkelibbey.org>
2009-01-08 21:04:59 -0600

    Joust is now require-able -- usable, even!

--- joust.el
+++ joust.el
@@ -1,15 +1,15 @@
-(unless (boundp '*emacs-config-path*)
-  (defvar *emacs-config-path* "~/.config.d/emacs"))
-(unless (boundp '*joust-path*)
-  (defvar *joust-path* (concat *emacs-config-path* "/joust")))
-(unless (boundp '*joust-packages-path)
-  (defvar *joust-packages-path* (concat *joust-path* "/packages")))
+(unless (boundp '*emacs-config-directory*)
+  (defvar *emacs-config-directory* "~/.emacs.d"))
+(unless (boundp '*joust-directory*)
+  (defvar *joust-directory* (concat *emacs-config-directory* "/joust")))
+(unless (boundp '*joust-packages-directory)
+  (defvar *joust-packages-directory* (concat *joust-directory* "/packages")))
 
 (defun joust:add-path (path)
-  (add-to-list 'load-path (concat *joust-packages-path* "/" path "/" path)))
+  (add-to-list 'load-path (concat *joust-packages-directory* "/" path "/" path)))
 
 (defun joust:list-packages ()
-  (let ((packages (directory-files *joust-packages-path*)))
+  (let ((packages (directory-files *joust-packages-directory*)))
     (remove "." (remove ".." packages))))
 
 (defun joust:require-packages (packages)
@@ -20,3 +20,6 @@
       (joust:require-packages (cdr packages)))))
 
 (joust:require-packages (joust:list-packages))
+
+(provide 'joust)
+
