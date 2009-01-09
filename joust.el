ab2bf55603545e46d8fb2c5c8f8841a220c88e16
Burke Libbey <burke@burkelibbey.org>
2009-01-08 21:22:33 -0600

    docstrings in joust.el

--- joust.el
+++ joust.el
@@ -6,13 +6,16 @@
   (defvar *joust-packages-directory* (concat *joust-directory* "/packages")))
 
 (defun joust:add-path (path)
+  "Add the specified package to the load path"
   (add-to-list 'load-path (concat *joust-packages-directory* "/" path "/" path)))
 
 (defun joust:list-packages ()
+  "Return a list of all installed packages"
   (let ((packages (directory-files *joust-packages-directory*)))
     (remove "." (remove ".." packages))))
 
 (defun joust:require-packages (packages)
+  "Given a list of package names, load all of them from the packages directory"
   (if (not (eq nil packages))
     (let ((package (car packages)))
       (joust:add-path package)
