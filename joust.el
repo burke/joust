c78ba22fa3186d0f3b91105686bc62e8286de103
Burke Libbey <burke@burkelibbey.org>
2009-01-08 21:36:42 -0600

    added header to joust.el

--- joust.el
+++ joust.el
@@ -1,3 +1,33 @@
+;;; joust.el --- A better package system for emacs
+
+;; Copyright (C) 2009 Chromium 53 <everyone@53cr.com>
+
+;; Author: Burke Libbey <burke@53cr.com>, Ryan Neufeld <ryan@53cr.com>
+;; Created: 08 Jan 2009
+;; Version: 0.1
+;; Keywords: tools
+
+;; This file is not (yet) part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+
 (unless (boundp '*emacs-config-directory*)
   (defvar *emacs-config-directory* "~/.emacs.d"))
 (unless (boundp '*joust-directory*)
