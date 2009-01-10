b2c28603cdf9a563d27e5843ad46d5425c132e0b
Burke Libbey <burke@burkelibbey.org>
2009-01-10 14:26:15 -0600

    Added some autoload functions

--- joust.el
+++ joust.el
@@ -11,12 +11,12 @@
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 2 of
 ;; the License, or (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public
 ;; License along with this program; if not, write to the Free
 ;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
@@ -25,6 +25,8 @@
 ;;; Code:
 
 (require 'cl) ;; yeah, deal with it.
+;; I need it for (flet), which makes the package description DSL work.
+;; Is there a better way to do this?
 
 (defmacro joust:defvar-maybe (var val)
   `(unless (boundp ',var)
@@ -46,12 +48,11 @@
 
 (defun joust:require-package (package)
   "Given a package name, load it from the packages directory"
-  (joust:add-path package)
-  (require (intern package)))
+  (joust:add-path package))
 
 (defun joust:initialize ()
   "Load all packages"
-  (mapcar 'joust:require-package (joust:list-packages)))
+  (mapcar 'joust:require-package (joust:list-installed)))
 
 (defun joust:byte-compile-package (package)
   (byte-recompile-directory (joust:package-path package) 0 t))
@@ -77,6 +78,7 @@
     (shell-command (concat "git clone \"" joust:current-package-url "\" \"" package-path "/" package "\"")))
 
   (joust:byte-compile-package package)
+  (joust:generate-autoloads (concat *joust-packages-directory* "/" package "/" package))
   (joust:require-package package))
 
 ;; TODO: Sanitize input so evil people can't nuke ~
@@ -98,9 +100,7 @@
   "List uninstalled, remote packages"
   (let ((packages (directory-files *joust-system-meta-directory*)))
     (mapcar '(lambda (el) el) ;; Remove trailing .el
-            (remove "." (remove ".." packages))))
-
-(joust:list-all)
+            (remove "." (remove ".." packages)))))
 
 (defun joust:list-out-of-date ()
   "List out of date local packages"
@@ -108,6 +108,38 @@
   ;; Insert Magic Here.
 
   )
+
+
+;; From Emacs 22.
+(defun joust:autoload-ensure-default-file (file)
+  "Make sure that the autoload file FILE exists and if not create it."
+  (unless (file-exists-p file)
+    (write-region
+     (concat ";;; " (file-name-nondirectory file)
+       " --- automatically extracted autoloads\n"
+       ";;\n"
+       ";;; Code:\n\n"
+       "\n;; Local Variables:\n"
+       ";; version-control: never\n"
+       ";; no-byte-compile: t\n"
+       ";; no-update-autoloads: t\n"
+       ";; End:\n"
+       ";;; " (file-name-nondirectory file)
+       " ends here\n")
+     nil file))
+  file)
+
+;; From package.el
+(defun joust:generate-autoloads (pkg-dir)
+  (let* ((auto-name "-autoloads.el")
+   (generated-autoload-file (concat pkg-dir auto-name))
+   (version-control 'never))
+    ;; In Emacs 22 'update-autoloads-from-directories' does not seem
+    ;; to be autoloaded...
+    (require 'autoload)
+    (unless (fboundp 'autoload-ensure-default-file)
+      (joust:autoload-ensure-default-file generated-autoload-file))
+    (update-autoloads-from-directories pkg-dir)))
 
 (joust:initialize)
 (provide 'joust)
