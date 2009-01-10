cbae708502870539c965faa9daf60868b81a06d8
Burke Libbey <burke@burkelibbey.org>
2009-01-10 13:51:01 -0600

    small changes to autoload

--- generate-autoloads.rb
+++ generate-autoloads.rb
@@ -8,12 +8,10 @@ autoload_triggers = []
 al=false
 package=nil
 file.each do |line|
-  autoload_triggers << line if al
+  autoload_triggers << (line.match(/\(defun ([^\s]+) \(/)[1]) if al
   al=false
   al=true if line =~ /;;;###autoload/
   package ||= (line.match(/\(provide ('.+)\)/)[1] rescue nil)
 end
 
-autoload_triggers.map!{ |el| el.split(/\s+/)[1] } # this is fragile; just do it better in elisp.
-
-puts autoload_triggers.map!{ |el| "(autoload #{package} \"#{el}\" t nil)" }
+puts autoload_triggers.map{ |el| "(autoload #{package} \"#{el}\" t nil)" }
